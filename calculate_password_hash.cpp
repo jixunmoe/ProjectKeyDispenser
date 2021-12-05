29cd84c1ee67f3b407faaa8b0b5015c68b58000a
Jixun Wu <jixun.moe@gmail.com>
2021-12-05 22:55:25 +0000

    chore: formatting for password hash calculator

--- calculate_password_hash.cpp
+++ calculate_password_hash.cpp
@@ -7,33 +7,32 @@
 #include "KeyDispenser/password_derive/admin_password.cpp"
 #include "KeyDispenser/password_derive/crc32.cpp"
 
-int main()
-{
+int main() {
   char password[18] = {};
   char salt[1024] = {};
   uint32_t iter = 1024;
 
-  do
-  {
+  do {
     fprintf(stderr, "// enter password (%d char): ", ADMIN_PASSWORD_LEN);
-    if (scanf("%17s", password) != 1) return 1;
+    if (scanf("%17s", password) != 1)
+      return 1;
   } while (strlen(password) != ADMIN_PASSWORD_LEN);
 
-  do
-  {
+  do {
     fprintf(stderr, "// enter salt (at least 8 char): ");
-    if (scanf("%1023s", salt) != 1) return 1;
+    if (scanf("%1023s", salt) != 1)
+      return 1;
   } while (strlen(salt) < 8);
 
-  do
-  {
+  do {
     fprintf(stderr, "// enter iter count (recommand: 1024; min: 128): ");
-    if (scanf("%u", &iter) != 1) return 1;
+    if (scanf("%u", &iter) != 1)
+      return 1;
   } while (iter < 128);
 
   auto hash = hash_admin_password(password, salt, strlen(salt), iter);
 
-  printf("// save output to: %s\n",
+  printf("// please save the following to: %s\n",
          "KeyDispenser/password_derive/password_config.local.h");
   printf("// password is: %s\n", password);
 
