ccb5c0088ae4848b62a4b22282474954337d933f
Jixun Wu <jixun.moe@gmail.com>
2021-12-05 20:02:58 +0000

    refactor: admin password hasher
    * Make VSCode happy, as we are targeting arduino's toolchain

--- KeyDispenser/password_derive/admin_password.cpp
+++ KeyDispenser/password_derive/admin_password.cpp
@@ -1,10 +1,12 @@
 #include "admin_password.h"
 #include "crc32.h"
 
+#ifndef htole32
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define htole32(value) (uint32_t{value})
 #else
 #define htole32(value) __builtin_bswap32(value)
+#endif
 #endif
 
 // Admin password hash calculation (little-endian):
@@ -13,7 +15,7 @@ uint32_t hash_admin_password(const char *password, const char *salt,
   uint32_t hash = crc32_proper(password, ADMIN_PASSWORD_LEN);
   hash = htole32(crc32(salt, salt_size, hash ^ 0xFFFFFFFF));
 
-  for (int i = 0; i < iter; i++) {
+  for (uint32_t i = 0; i < iter; i++) {
     hash = crc32_proper(&hash, sizeof(hash));
     hash = htole32(crc32(salt, salt_size, hash ^ 0xFFFFFFFF));
   }
--- calculate_password_hash.cpp
+++ calculate_password_hash.cpp
@@ -1,6 +1,5 @@
-#include <iomanip>
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 // use mocks
 #define VSCODE_COMPAT
@@ -8,40 +7,40 @@
 #include "KeyDispenser/password_derive/admin_password.cpp"
 #include "KeyDispenser/password_derive/crc32.cpp"
 
-int main() {
-  std::string password;
-  std::string salt;
+int main()
+{
+  char password[18] = {};
+  char salt[1024] = {};
   uint32_t iter = 1024;
 
-  do {
-    std::cout << "// enter password (" << ADMIN_PASSWORD_LEN << " char): ";
-    std::cin >> password;
-  } while (password.length() != ADMIN_PASSWORD_LEN);
+  do
+  {
+    fprintf(stderr, "// enter password (%d char): ", ADMIN_PASSWORD_LEN);
+    if (scanf("%17s", password) != 1) return 1;
+  } while (strlen(password) != ADMIN_PASSWORD_LEN);
 
-  do {
-    std::cout << "// enter salt (at least 8 char): ";
-    std::cin >> salt;
-  } while (salt.length() < 8);
+  do
+  {
+    fprintf(stderr, "// enter salt (at least 8 char): ");
+    if (scanf("%1023s", salt) != 1) return 1;
+  } while (strlen(salt) < 8);
 
-  do {
-    std::cout << "// enter iter count (recommand: 1024; min: 128): ";
-    std::cin >> iter;
+  do
+  {
+    fprintf(stderr, "// enter iter count (recommand: 1024; min: 128): ");
+    if (scanf("%u", &iter) != 1) return 1;
   } while (iter < 128);
 
-  auto hash =
-      hash_admin_password(password.c_str(), salt.c_str(), salt.length(), iter);
+  auto hash = hash_admin_password(password, salt, strlen(salt), iter);
 
-  std::cout << "// save output to: "
-               "KeyDispenser/password_derive/password_config.local.h"
-            << std::endl;
-  std::cout << "// password: " << password << std::endl;
+  printf("// save output to: %s\n",
+         "KeyDispenser/password_derive/password_config.local.h");
+  printf("// password is: %s\n", password);
 
-  std::cout << "#define ADMIN_PWD_SALT R\"_(" << salt << ")_\"" << std::endl;
-  std::cout << "#define ADMIN_PWD_SALT_SIZE (" << salt.length() << ")"
-            << std::endl;
-  std::cout << "#define ADMIN_PASSWORD_ITER (" << iter << ")" << std::endl;
-  std::cout << "#define ADMIN_CRC32_HASH (" << std::setfill('0') << std::setw(8)
-            << hash << ")" << std::endl;
+  printf("#define ADMIN_PWD_SALT R\"_(%s)_\"\n", salt);
+  printf("#define ADMIN_PWD_SALT_SIZE (%ld)\n", strlen(salt));
+  printf("#define ADMIN_PASSWORD_ITER (%u)\n", iter);
+  printf("#define ADMIN_CRC32_HASH (0x%x)\n", hash);
 
   return 0;
 }
